f1a09921f37caff5020ab27945f1dbfb7f117892
Benadette Mokandu <benadettemokandu@gmail.com>
2025-11-07 13:36:30 +0300

    app.jsx added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Chat from "./pages/Chat";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
